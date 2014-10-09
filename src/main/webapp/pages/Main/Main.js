ebd938207dadc18a87ab8b0ef90e146523ec8000
Srikanth Subramanian <srikanth.subramanian@wavemaker.com>
2014-10-09 13:33:07 +0530

    update

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -1,23 +1,25 @@
-Application.$controller("MainPageController", ['$scope', function ($scope) {
-    "use strict";
+Application.$controller("MainPageController", ['$scope',
+    function($scope) {
+        "use strict";
 
-    /* perform any action with the variables inside this block(on-page-load) */
-    $scope.onPageVariablesReady = function () {
-        /*
-         * variables can be accessed through '$scope.Variables' property here
-         * e.g. $scope.Variables.staticVariable1.getData()
-         */
-    };
-    /* perform any action with widgets inside this block */
-    $scope.onPageReady = function () {
-        /*
-         * widgets can be accessed through '$scope.Widgets' property here
-         * e.g. $scope.Widgets.byId(), $scope.Widgets.byName()or access widgets by $scope.Widgets.widgetName
-         */
-    };
-    
-    $scope.greeting = "ABCDEFGHI";
-    $scope.greeting_new = "ABCDEFGHIJKLMNO";
-    $scope.greeting_new_new = "ABCDEFGHIJKLMNOPQRST";
+        /* perform any action with the variables inside this block(on-page-load) */
+        $scope.onPageVariablesReady = function() {
+            /*
+             * variables can be accessed through '$scope.Variables' property here
+             * e.g. $scope.Variables.staticVariable1.getData()
+             */
+        };
+        /* perform any action with widgets inside this block */
+        $scope.onPageReady = function() {
+            /*
+             * widgets can be accessed through '$scope.Widgets' property here
+             * e.g. $scope.Widgets.byId(), $scope.Widgets.byName()or access widgets by $scope.Widgets.widgetName
+             */
+        };
 
-}]);
+        $scope.greeting = "ABCDEFGHI";
+        $scope.greeting_new = "ABCDEFGHIJKLMNO";
+        $scope.greeting_new_new = "ABCDEFGHIJKLMNOPQRST";
+
+    }
+]);
