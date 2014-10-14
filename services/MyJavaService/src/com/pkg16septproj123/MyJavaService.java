a5492786a829143ae41feb4633c0246e22d54758
Srikanth Subramanian <srikanth.subramanian@wavemaker.com>
2014-10-14 15:18:08 +0530

    New files added - [EmployeeRepository.java,HrdbDepartmentController.java,User.java,DepartmentService.java,DepartmentServiceImpl.java,HrdbQueryExecutionController.java,UserService.java,UserServiceImpl.java,hrdb-queries.hbm.xml,HrdbUserController.java,VacationServiceImpl.java,servicedef.xml,Vacation.java,EmployeeServiceImpl.java,VacationRepository.java,DepartmentRepository.java,QueryExecutorServiceImpl.java,MyJavaService.smd,MyJavaService.java,Department.java,UserRepository.java,servicedef.xml,HrdbEmployeeController.java,Employee.java,hrdb_published_dataModel.json,hrdb.spring.xml,hrdb_query.json,MyJavaService.spring.xml,HrdbVacationController.java,hrdb.smd,EmployeeService.java,hrdb.properties,VacationService.java,QueryExecutorService.java] Files modified - [hrdb.script,hrdb.properties,app.variables.json] Refactored files - [] Fixed bugs - [] Removed files - []

---
+++ services/MyJavaService/src/com/pkg16septproj123/MyJavaService.java
@@ -0,0 +1,42 @@
+package com.pkg16septproj123;
+
+import com.wavemaker.runtime.javaservice.JavaServiceSuperClass;
+import com.wavemaker.runtime.service.annotations.ExposeToClient;
+
+/**
+ * This is a client-facing service class.  All
+ * public methods will be exposed to the client.  Their return
+ * values and parameters will be passed to the client or taken
+ * from the client, respectively.  This will be a singleton
+ * instance, shared between all requests. 
+ * 
+ * To log, call the superclass method log(LOG_LEVEL, String) or log(LOG_LEVEL, String, Exception).
+ * LOG_LEVEL is one of FATAL, ERROR, WARN, INFO and DEBUG to modify your log level.
+ * For info on these levels, look for tomcat/log4j documentation
+ */
+@ExposeToClient
+public class MyJavaService extends JavaServiceSuperClass {
+    /* Pass in one of FATAL, ERROR, WARN,  INFO and DEBUG to modify your log level;
+     *  recommend changing this to FATAL or ERROR before deploying.  For info on these levels, look for tomcat/log4j documentation
+     */
+    public MyJavaService() {
+       super(INFO);
+    }
+
+    public String sampleJavaOperation() {
+       String result  = null;
+       try {
+          log(INFO, "Starting sample operation");
+          result = "Hello World";
+          log(INFO, "Returning " + result);
+       } catch(Exception e) {
+          log(ERROR, "The sample java service operation has failed", e);
+       }
+       return result;
+    }
+    
+    public boolean isTest() {
+        return true;
+    }
+
+}
---
+++ services/hrdb/src/com/hrdb/service/QueryExecutorService.java
@@ -0,0 +1,20 @@
+
+package com.hrdb.service;
+// Generated 14 Oct, 2014 3:13:40 PM 
+
+import java.util.Map;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.wavemaker.runtime.data.model.CustomQuery;
+import com.wavemaker.runtime.data.exception.QueryParameterMismatchException;
+
+public interface QueryExecutorService {
+
+	
+	Page<Object> executeWMCustomQuerySelect(CustomQuery query, Pageable pageable) ;
+
+	int executeWMCustomQueryUpdate(CustomQuery query) ;
+}
+
